074ad47e925b494543748105e1a5aa70d7c463a2
Juan-Gines <JuangAlenta@hotmail.com>
2024-06-25 13:48:57 +0200

    Client astro finished

--- client/src/components/forms/ServerForm.jsx
+++ client/src/components/forms/ServerForm.jsx
@@ -42,7 +42,7 @@ const ServerForm = () => {
 	return (
 		<form
 			onSubmit={handleSubmit}
-			className='max-w-sm mx-auto'
+			className='max-w-sm mx-auto mb-7'
 		>
 			<div className='flex gap-4'>
 				{serverSelectData.map((select, i) => (
@@ -55,7 +55,7 @@ const ServerForm = () => {
 					/>
 				))}
 			</div>
-			{error && <p className='text-red-500 text-sm pb-4'>*{error}</p>}
+			{error && <p className='text-red-500 text-sm text-center pb-4'>*{error}</p>}
 			<div className='flex place-content-center'>
 				<Button title={servBbddButton} />
 			</div>
---
+++ client/src/data/products.js
@@ -0,0 +1,17 @@
+export const productList = [
+	{
+		id: 1,
+		name: 'Patatas',
+		onBasket: true,
+	},
+	{
+		id: 2,
+		name: 'Arroz',
+		onBasket: false,
+	},
+	{
+		id: 3,
+		name: 'Lentejas',
+		onBasket: false,
+	},
+];
